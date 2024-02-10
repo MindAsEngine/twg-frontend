b40aec067d0102932ceb75386e3746d1f1333767
Ivan Zubkov <thundiverter@gmail.com>
2024-02-10 19:30:10 +0300

    Доработка компонентов "Feature" и "NoTur"
    - Добавил изображения: featureVector.svg, NoTurBackground.png;
    - Добавил переменные Fs48, Fw600 в variables.module.scss;
    - Добавил классы .fs48, .fw600 в style.module.scss;
    - Заменил изображение в Feature (сделал фоновым для адаптивности);
    - Закончил компонент NoTur;
    - Сделал кнопку "Вход" в шапке капсуловидной;
    - Изменил z-index шапки (2 -> 3), чтобы ui карты не был поверх него.

--- src/components/feature/Feature.js
+++ src/components/feature/Feature.js
@@ -1,53 +1,31 @@
-import FeatureImg from "../../img/feature.png";
 import { Link } from "react-router-dom";
-
-import { handleDragStart } from "../../app/function";
 import "./feature.scss";
 
+// import FeatureImg from "../../img/featureVector.svg";
+// import { handleDragStart } from "../../app/function";
 
 const Feature = () => {
   return (
-    <div className="feature flex" style={{ position: "relative" }}>
+    <div className="feature flex container" style={{ position: "relative" }}>
       <div
         className="flex justif-ss-cent"
-        style={{
-          position: "absolute",
-          top: "0",
-          left: "0",
-          right: "0",
-          bottom: "0",
-        }}
       >
         <div className="container" style={{ width: "100%" }}>
           <div className="feature__content">
-            <div className="content__title f-cWh fw700 fs96 lh123">
+            <div className="content__title f-cWh fw700 fs48">
               <p>Собери свой тур</p>
             </div>
             <div className="content__text f-cWh fw400 fs32 lh41">
               <p>
-                Вы можете оставить заявку и мы подберём лучший вариант
-                специально для вас
+                Вы можете оставить заявку и мы подберём лучший вариант специально для вас
               </p>
             </div>
-            <button className="content__button bgYl fw400 fs32 lh41">
+            <button className="content__button bgYl fw600 fs24 lh41">
               <Link to="/constructor">Собрать тур</Link>
             </button>
           </div>
         </div>
       </div>
-      <img
-        onDragStart={handleDragStart}
-        className="feature__img"
-        src={FeatureImg}
-        alt="#"
-        style={{
-          marginLeft: "auto",
-          maxHeight: "549px",
-          maxWidth: "1032px",
-          width: "100%",
-          height: "100%",
-        }}
-      />
     </div>
   );
 };
--- src/components/noTur/NoTur.js
+++ src/components/noTur/NoTur.js
@@ -1,9 +1,33 @@
-const NoTur = () => {
-    return(
-        <div className="notur">
+// import NoturImg from "../../img/NoTurBackground.png";
+import { Link } from "react-router-dom";
+import "./notur.scss";
 
+// import { handleDragStart } from "../../app/function";
+
+const NoTur = () => {
+  return (
+    <div className="notur flex" style={{ position: "relative" }}>
+      <div
+        className="flex justif-ss-cent"
+      >
+        <div className="container" style={{ width: "100%" }}>
+          <div className="notur__content container">
+            <div className="content__title f-cWh fw700 fs48">
+              <p>Не нашли что искали?</p>
+            </div>
+            <div className="content__text f-cWh fw400 fs32 lh41">
+              <p>
+                Заполните эту форму и наши агенты помогут вам lorem ipsum dolor sit amet
+              </p>
+            </div>
+            <button className="content__button bgYl fw600 fs24 lh41">
+              <Link to="/constructor">Заказать обратный звонок</Link>
+            </button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default NoTur;
